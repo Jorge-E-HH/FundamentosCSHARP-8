35ae9d1915b62f008e201701cbe94ca7cd9da0d7
ThethingOne <Jaydersmk@gmail.com>
2022-12-26 14:21:39 -0600

    Json

---
+++ FundamentosCSHARP3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using FundamentosCSHARP.Models;
+using FundamentosCSHARP3.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace FundamentosCSHARP3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            HttpClient client = new HttpClient();
+
+            var httpResponse = await client.GetAsync(url);
+
+            if(httpResponse.IsSuccessStatusCode)
+            {
+                var content = await httpResponse.Content.ReadAsStringAsync();
+                List<Models.Post> posts =
+                 JsonSerializer.Deserialize<List<Models.Post>>(content);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Curso c# nobb/Master. 
+# leccion 8. 
+#Objeto, persona/herramientas,bla, bla.
+#Interfaces.
+#Serializado/Descerializado.
+#Json/object txt/bin/crear txt, desde c#/cargar desde txt.
+
+# Desarrollado en C#
+
+ # Créditos
+  [J. Hernandez](https://github.com/Jorge-E-HH) autor original del contenido.
